9dc55c03d11fc3f90ce1fd320f137c707bb986d4
amaan <17208409+amaansaeed@users.noreply.github.com>
2019-02-11 18:33:54 -0500

    adds firebase support

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-//  Dependencies
-const express = require('express')
-const app = express()
-const path = require('path')
-require('dotenv').config()
-
-//  Socket.io
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
-
 //  Third-Party Express Middleware
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 
-//  Routes
-const getRoutes = require('./routes/get_routes')
-const postRoutes = require('./routes/post_routes')
+//  Dependencies
+const path = require('path')
+require('dotenv').config()
+
+//  Express | Socket.io
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
 
 //  Connect to MongoDB
 require('./controllers/connect')
 
-//  socket.io
-io.on('connection', socket => {
-  console.log('socket.io: new user connected...')
-  // io.emit('news', { hello: 'world' })
-})
+//  Routes
+const getRoutes = require('./routes/get_routes')
+const postRoutes = require('./routes/post_routes')
 
 //  Middleware
 //  third-party
@@ -32,35 +25,38 @@ app.use(express.json()) // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })) // to support URL-encoded bodies
 app.use(morgan('tiny')) // request logger
 app.use(helmet()) //  third-party middleware
-app.use(cors())
+app.use(cors({ credentials: true, origin: true }))
 app.use((req, res, next) => {
   req.io = io
   next()
 })
 
+//  socket.io
+const io = require('socket.io')(server, {
+  path: '/socket/v1/drink-exchange'
+})
+io.on('connection', socket => {
+  console.log('"Drink Exchange" - socket.io: new user connected...')
+})
+
 //  API Routing
-app.use('/api/v1', getRoutes)
-app.use('/api/v1', postRoutes)
+app.use('/api/v1/drink-exchange', getRoutes)
+app.use('/api/v1/drink-exchange', postRoutes)
 
 // Serve the static files from the React app
-app.use(
-  '/static',
-  express.static(path.join(__dirname, './client/build/static'))
-)
-app.get('/favicon.ico', function(req, res) {
-  res.sendFile('favicon.ico', { root: path.join(__dirname, './client/build/') })
-})
-app.get('*', function(req, res) {
-  res.sendFile('index.html', { root: path.join(__dirname, './client/build/') })
-})
+// app.use(
+//   '/static',
+//   express.static(path.join(__dirname, './client/build/static'))
+// )
+// app.get('/favicon.ico', function(req, res) {
+//   res.sendFile('favicon.ico', { root: path.join(__dirname, './client/build/') })
+// })
+// app.get('*', function(req, res) {
+//   res.sendFile('index.html', { root: path.join(__dirname, './client/build/') })
+// })
 
-//  Port & Listener
-app.set('port', process.env.PORT)
-
-//  listening through socket.io and indirectly express
-server.listen(app.get('port'), () => {
-  console.log(`Drink-Exchange server listening on port: ${app.get('port')}`)
-})
+//  PORT LISTENSER
+server.listen(process.env.PORT)
 
 //  DEVELOPMENT
 // require("./seed/seed")
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,9 @@
-import io from "socket.io-client"
+import io from 'socket.io-client'
 
 //  env variables
 const endpoint = process.env.REACT_APP_SOCKETIO_ENDPOINT
+const path = process.env.REACT_APP_SOCKETIO_PATH + '/socket.io'
 
-export default io(endpoint)
+export default io(endpoint, {
+  path: path
+})
